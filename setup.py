793bd0bb3e07d65bc41f30042d34b84cd7429241
Sourav Banerjee <sourav692@gmail.com>
2026-04-08 16:35:13 +0530

    Update author information in setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name= "medical-chatbot",
     version= "0.1.0",
-    author= "Boktiar Ahmed Bappy",
-    author_email="entbappy73@gmail.com",
+    author= "Sourav Banerjee",
+    author_email="sourav692@gmail.com",
     packages= find_packages(),
     intstall_requires=[]
 )
